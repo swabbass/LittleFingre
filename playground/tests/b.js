c87b68d8604ef9c92da2f1566c88feaa3a6bf3a6
Ward Abbass <warda@wix.com>
2019-08-19 13:16:18 +0300

    make it easy to run multiple tests with sleep in them to simulate heavy work

--- playground/tests/b.js
+++ playground/tests/b.js
@@ -1,41 +1,17 @@
 const {expect, it, xit, end} = require('LittleFinger');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
-it("should it 1==1 is true", () => {
-    expect(1 === 1);
-});
-
-it("should it 1 !== 2 is true", () => {
-    expect(1 !== 2);
-});
-
-it("should it 0/5 === 0 is true", () => {
-    expect(1 === 1);
-});
-
-it("should it 0/5 === 0 is true1", () => {
-    expect(1 === 1);
-});
-
-it("should it 0/5 === 0 is true2", () => {
-    expect(1 === 1);
-});
-
-it("should it 0/5 === 0 is true3", () => {
-    expect(1 === 1);
-});
-
-it("should it 0/5 === 0 is true4", () => {
-    expect(1 === 1);
-});
-
-for (let i = 0; i < 50; i++) {
-    it(`should it 0/5 === 0 is true ${i}`, () => {
+for (let i = 0; i < 100; i++) {
+    it(`should it 0/5 === 0 is true ${i}`,async () => {
+       await sleep(1000);
         expect(1 === 1);
     });
 }
 
-xit("should xit not work", () => {
-    expect(1 === 2);
-});
-
+// xit("should xit not work", () => {
+//     expect(1 === 2);
+// });
+//
