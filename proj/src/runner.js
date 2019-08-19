3a254fc8ee430cc99d5f5d14c4a1832f1c7588ab
Ward Abbass <warda@wix.com>
2019-08-19 13:17:08 +0300

    small refactoring

--- proj/src/runner.js
+++ proj/src/runner.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node --experimental-worker
-const path = require('path');
+const colors = require('colors');
 
 global.LilRunner = {
     config: {
@@ -9,46 +9,53 @@ global.LilRunner = {
 
 global.__execution = 'serial';
 
-var ArgumentParser = require('argparse').ArgumentParser;
-var parser = new ArgumentParser({
-    addHelp: true,
-    description: 'Little Finger Runner'
-});
-parser.addArgument(
-    ['--files'],
-    {
-        nargs: '+',
-        help: 'list of test files'
-    }
-);
-parser.addArgument(
-    ['--config'],
-    {
-        help: '.json file as a config file to be injected to the tests suites'
-    }
-);
-parser.addArgument(
-    ['--parallel'],
-    {
-        help: 'run tests int parallel',
-        action: 'storeTrue'
-    }
-);
+function initArgParser() {
+    var ArgumentParser = require('argparse').ArgumentParser;
+    var parser = new ArgumentParser({
+        addHelp: true,
+        description: 'Little Finger Runner'
+    });
+    parser.addArgument(
+        ['--files'],
+        {
+            nargs: '+',
+            help: 'list of test files'
+        }
+    );
+    parser.addArgument(
+        ['--config'],
+        {
+            help: '.json file as a config file to be injected to the tests suites'
+        }
+    );
+    parser.addArgument(
+        ['--parallel'],
+        {
+            help: 'run tests int parallel',
+            action: 'storeTrue'
+        }
+    );
+    return parser;
+}
 
+var parser = initArgParser();
 
 const __args = parser.parseArgs();
 const __cwd = process.cwd();
 const __testSuites = __args.files;
 const __suitesConfig = __args.config;
 const __testSuitePaths = __testSuites.map(s => `${__cwd}/${s}`);
-const {Worker} = require('worker_threads');
 const itsPerSuite = {};
 
+
 function createWorkerAndWaitForMessages(id, workerData) {
+    const path = require('path');
+    const {Worker} = require('worker_threads');
     const worker = new Worker(path.resolve(`${__dirname}/suite_worker.js`), {workerData});
     worker.on('message', (message) => {
-        console.log(`MainREC-${id}: ` + JSON.stringify(message));
         const {action, data} = message;
+        console.log(`MainREC-${id}: ${action}`);
+
         switch (action) {
             case 'ready':
                 if (!itsPerSuite[workerData.suitePath]) {
@@ -58,8 +65,9 @@ function createWorkerAndWaitForMessages(id, workerData) {
                 const itToRun = itsPerSuite[workerData.suitePath].pop();
                 if (itToRun) {
                     worker.postMessage({action: 'run', data: itToRun, worker: id});
+                } else {
+                    console.log(`Worker-${id} Finished and waiting`.rainbow);
                 }
-                console.log("finished");
                 break;
             case 'finished':
                 break;
@@ -70,14 +78,16 @@ function createWorkerAndWaitForMessages(id, workerData) {
 }
 
 function runService(workerData) {
-    for (let i = 0; i < 2; i++) {
+    const os = require('os');
+    const cpuCount = os.cpus().length;
+    console.log(`Running with ${cpuCount} Workers`.bgMagenta.black);
+    for (let i = 0; i < cpuCount; i++) {
         createWorkerAndWaitForMessages(i, workerData);
     }
 }
 
 
 console.log(JSON.stringify(__testSuitePaths));
-// console.log(`${__cwd}/${__testSuites[0]}`);
 if (__args.parallel) {
     global.__execution = 'parallel';
     console.log('parallel not support');
